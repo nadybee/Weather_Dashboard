ac907f8d23445a3e0fe0de7691bf6f8d6b0b8340
Nadybee <natalie@yoodlize.com>
2022-05-18 16:19:10 -0600

    remove key

--- Develop/script.js
+++ Develop/script.js
@@ -3,71 +3,66 @@ const todayDate = moment(now).format("dddd, MMMM Do, h:mm a z")
 const currentHour = moment(now).format("H")
 
 console.log(todayDate)
-let apiKey = `81764070bcb422c925d6bb91e367c480`
-let lat = 33.44
-let lon = -90.04
+let apiKey = ``
+let lat =40.2338
+let lon=-111.6585
+let city= "Provo, UT"
+
+function findLocation () {
+    $("#city-buttons").click(event =>{
+        if (event.target.id==='austin'){
+            city = "Austin"
+            lat = 30.2672
+            lon= -97.7431
+        }
+
+    })
+}
+
+// const URL = `https://api.openweathermap.org/data/2.5/onecall/zip?zio&exclude=minutely,hourly&appid=${apiKey}&units=imperial`
 const URL=   `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=imperial`
 let cardHTML = []
+let jumbotronHTML;
 let timezone;
 
 const fetchWeather=(URL) => {
+ 
 fetch(URL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
+    // jumbotronHTML=`<h3 class="text-dark> ${city} (${data.current.dt})</h3> <h4> Temp:${data.current.temp}°</h4> <h4> Wind:  </h4>`
+
+    $("#city-buttons").click(event =>{
+        if (event.target.id==='austin'){
+            city = "Austin"
+            lat = 30.2672
+            lon= -97.7431
+            console.log('austin')
+        }
+
+    })
       console.log(data)
+
   for (let i=0;i<5; i++){
-      console.log(data.daily[i].dt)
-      console.log(data.daily[i].temp.day)
-      console.log(data.daily[i].wind_speed)
-      console.log(data.daily[i].humidity)
     
         cardHTML.push(`
-        <div class="card bg-dark text-white">
-        <div class="card-title p-2"> </div>
+        <div class="card text-white background">
+        <div class="card-title p-2"> ${data.daily[i].dt} </div>
         <div class="card-body">
-            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="mx-auto" >
-            <div class="card-text"> Temp: ${data.daily[i].temp.day}</div>
-            <div class="card-text"> Wind: ${data.daily[i].wind_speed} </div>
-            <div class="card-text"> Humidity: ${data.daily[i].wind_speed} </div>
+            <img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png" alt="weather icon" class="mx-auto" >
+            <div class="card-text"> Temp: ${Math.round(data.daily[i].temp.day)}°</div>
+            <div class="card-text"> Wind: ${Math.round(data.daily[i].wind_speed)} mph</div>
+            <div class="card-text"> Humid: ${data.daily[i].humidity}%</div>
         </div>
     </div>`)
       } 
       document.querySelector(".card-deck").innerHTML = cardHTML.join('')
-    
+      document.getElementById('jumbotron').innerHTML = jumbotronHTML
+
     }) 
-  
+
 }
- 
-
-// async function fetchWeather () {
-//   const response = await fetch(
-//     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=imperial`
-//   )
-//  const data = await response.json()
-// console.log(data)
-// }
-
-
-
-
-// const weatherCardHTML = () => {
-    
-//   for (let i = 0; i < 5; i++) {
-//     cardHTML.push(`
-//     <div class="card bg-dark text-white">
-//     <div class="card-title p-2"> data </div>
-//     <div class="card-body">
-//         <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="mx-auto" >
-//         <div class="card-text"> Temp: </div>
-//         <div class="card-text"> Wind: </div>
-//         <div class="card-text"> Humidity </div>
-//     </div>
-// </div>`)
-//   }
-// }
 fetchWeather(URL)
-// weatherCardHTML()
 
-document.querySelector(".card-deck").innerHTML = cardHTML
